9ce06e7ba7a48ede02cc2895e453f250866ec8d7
alishaayyyy <alisha12740@gmail.com>
2026-04-08 12:39:25 -0700

    work-sort

--- mybindle/vite.config.ts
+++ mybindle/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // 👈 CHANGE THIS FROM '/' to './'
+   base: '/mybindle/',   // 👈 CHANGE THIS FROM '/' to './'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',  // 👈 Add this
