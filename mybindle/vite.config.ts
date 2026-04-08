843534ee0b8b6b6c17872f564abb6a14e324fb42
alishaayyyy <alisha12740@gmail.com>
2026-04-08 12:33:21 -0700

    work

--- mybindle/vite.config.ts
+++ mybindle/vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ["react", "react-dom"],
+  base: './',  // 👈 CHANGE THIS FROM '/' to './'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',  // 👈 Add this
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
   },
-});
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") }
+  }
+})
